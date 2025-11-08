10c72365a153e19e1aeb0b36ec4b67ff74c45bde
Hyvalker Allan Correia do Amaral <hyva.allan@gmail.com>
2025-11-08 19:20:25 -0300

    feat: implement Task class with getters, setters and constructor

--- TaskManager/src/model/Task.java
+++ TaskManager/src/model/Task.java
@@ -1,4 +1,68 @@
 package model;
 
+import java.time.LocalDate;
+
 public class Task {
+    private int id;
+    private String title;
+    private String description;
+    private LocalDate deadline;
+    private boolean isDone;
+    private int priority;
+
+    public Task (int id, String title, String description, LocalDate deadline, boolean isDone, int priority) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.deadline = deadline;
+        this.isDone = isDone;
+        this.priority = priority;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    public int getId() {
+        return id;
+    }
+
+    public void setDeadline(LocalDate deadline) {
+        this.deadline = deadline;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getDeadline() {
+        return deadline;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    public boolean isDone() {
+        return isDone;
+    }
+
+    public void setDone(boolean done) {
+        isDone = done;
+    }
 }
