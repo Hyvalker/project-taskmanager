0820cd49d5146cae1008abab5a1b5f7cefebe5ba
Hyvalker Allan Correia do Amaral <hyva.allan@gmail.com>
2025-11-10 16:38:59 -0300

    Fix: Fix InputMismatchException in Main

--- TaskManager/src/app/Main.java
+++ TaskManager/src/app/Main.java
@@ -2,6 +2,7 @@ package app;
 
 import services.Manager;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
@@ -12,30 +13,35 @@ public class Main {
         boolean running = true;
 
         while(running) {
-            System.out.println("\n--- Lista de Tarefas ---");
-            System.out.println("1 - Criar nova tarefa");
-            System.out.println("2 - Carregar tarefas salvas");
-            System.out.println("3 - Lista todas as tarefas");
-            System.out.println("4 - Buscar tarefa");
-            System.out.println("5 - Editar tarefa");
-            System.out.println("6 - Remover tarefa da lista");
-            System.out.println("0 - Sair");
+            try {
+                System.out.println("\n--- Lista de Tarefas ---");
+                System.out.println("1 - Criar nova tarefa");
+                System.out.println("2 - Carregar tarefas salvas");
+                System.out.println("3 - Lista todas as tarefas");
+                System.out.println("4 - Buscar tarefa");
+                System.out.println("5 - Editar tarefa");
+                System.out.println("6 - Remover tarefa da lista");
+                System.out.println("0 - Sair");
 
-            int option = scanner.nextInt();
-            scanner.nextLine();
+                int option = scanner.nextInt();
+                scanner.nextLine();
 
-            switch(option) {
-                case 1 -> manager.createTask(scanner);
-                case 2 -> manager.loadTaskList();
-                case 3 -> manager.listTasks();
-                case 4 -> manager.searchTask(scanner);
-                case 5 -> manager.editTask(scanner);
-                case 6 -> manager.removeTask(scanner);
-                case 0 -> {
-                    System.out.println("Encerrando o programa.");
-                    running = false;
+                switch (option) {
+                    case 1 -> manager.createTask(scanner);
+                    case 2 -> manager.loadTaskList();
+                    case 3 -> manager.listTasks();
+                    case 4 -> manager.searchTask(scanner);
+                    case 5 -> manager.editTask(scanner);
+                    case 6 -> manager.removeTask(scanner);
+                    case 0 -> {
+                        System.out.println("Encerrando o programa.");
+                        running = false;
+                    }
+                    default -> System.out.println("Opção inválida. Tente novamente.");
                 }
-                default -> System.out.println("Opção inválida. Tente novamente.");
+            } catch (InputMismatchException e) {
+                System.out.println("Entrada inválida. Tente novamente.");
+                scanner.nextLine();
             }
         }
         scanner.close();
